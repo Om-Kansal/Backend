6bc45aa227923936f0c44bb6056666f6c6f596d7
Om Kansal <omkansal285@gmail.com>
2026-02-21 11:11:04 +0530

    api handeling

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ dotenv.config({
 });
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED", err);
+    
+})
